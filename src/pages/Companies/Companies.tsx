dc46cc77d8f0ec4c82211e3b0017e1e388a9c0ac
Alex Zakharov <zakharov.products@gmail.com>
2024-04-12 15:31:15 +0300

    test_request_to_fake_api

--- src/pages/Companies/Companies.tsx
+++ src/pages/Companies/Companies.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import st from './Companies.module.scss';
+import { useDispatch } from 'react-redux';
+import { companiesFetched } from '../../actions';
 
 
 export const Companies = (): JSX.Element => {
+
+  const dispatch = useDispatch(); 
+
+  useEffect(() => {
+    fetch('http://localhost:3001/companies')
+      .then(response => response.json())
+      .then(data => {
+        dispatch(companiesFetched(data));
+      })
+  }, [])
+
   return (
     <main className={st.companies}>
       TestPageCompany
