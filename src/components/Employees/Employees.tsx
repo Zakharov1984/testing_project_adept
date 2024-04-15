903965885c41100bfe02c99d60ba4afc51d27ff3
Alex Zakharov <zakharov.products@gmail.com>
2024-04-15 12:31:55 +0300

    moved_request_to_component_ Employees

--- src/components/Employees/Employees.tsx
+++ src/components/Employees/Employees.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
+
 import { EmployeesList } from '../EmployeesList/EmployeesList';
+import { useAppDispatch } from '../../hooks/hook';
+import { fetchEmployees } from '../EmployeesList/employeesSlice';
 
 export const Employees = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchEmployees());
+  }, [])
+
   return (
     <section className="employees">
       <EmployeesList/>
--- src/components/EmployeesList/EmployeesList.tsx
+++ src/components/EmployeesList/EmployeesList.tsx
@@ -1,19 +1,11 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/hook";
-import { fetchEmployees } from "./employeesSlice";
 import { EmployeesTableItem } from '../EmployeesTableItem/EmployeesTableItem';
 
 export const EmployeesList = () => {
-  const dispatch = useAppDispatch(); 
-  const { employees, 
-          isFirstActivatedCompany, 
-          employeesLoadingStatus } = useAppSelector(
+  const { employees } = useAppSelector(
     (state) => state.employees
   );
-
-  useEffect(() => {
-    dispatch(fetchEmployees());
-  }, [])
 
   const isActiveAllEmployees = () => {
 
@@ -24,34 +16,35 @@ export const EmployeesList = () => {
   }
 
   return (
-    <table className="bg-table">
-      <caption>
-        <label>
-          Выделить все
-          <input
-            className="chbCenter" 
-            type="checkbox"
-            name="allEmployees"
-            checked={false}/>
-        </label>
-      </caption>
-      <thead>
-        <tr>
-          <td>Чекбокс</td>
-          <td>Фамилия</td>
-          <td>Имя</td>
-          <td>Должность</td>
-        </tr>
-      </thead>
-      <tbody>
-        
-      </tbody>
-    </table>
+    <article>
+      <h3>Имя Компании</h3>
+      <table className="bg-table">
+        <caption>
+          <label>
+            Выделить все
+            <input
+              className="chbCenter" 
+              type="checkbox"
+              name="allEmployees"
+              checked={false}/>
+          </label>
+        </caption>
+        <thead>
+          <tr>
+            <td>Чекбокс</td>
+            <td>Фамилия</td>
+            <td>Имя</td>
+            <td>Должность</td>
+          </tr>
+        </thead>
+        <tbody>
+          {employees.Company0?.map(
+              employee => <EmployeesTableItem 
+                key={employee.id} 
+                {...employee}/>
+          )}
+        </tbody>
+      </table>
+    </article>
   )
 }
-
-/* {employees.Company0.map(
-          employee => <EmployeesTableItem 
-            key={employee.id} 
-            {...employee}/>
-        )} */
